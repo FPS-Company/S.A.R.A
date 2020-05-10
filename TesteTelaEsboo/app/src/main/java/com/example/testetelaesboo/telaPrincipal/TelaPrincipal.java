5c3b1cbced4867fcc63453953bc9055f5f2cf2e0
Bruxonyr <d.s.martins17@gmail.com>
2020-05-10 20:07:23 -0300

    Tela principal versão 3

--- TesteTelaEsboo/app/src/main/java/com/example/testetelaesboo/telaPrincipal/TelaPrincipal.java
+++ TesteTelaEsboo/app/src/main/java/com/example/testetelaesboo/telaPrincipal/TelaPrincipal.java
@@ -4,21 +4,98 @@ package com.example.testetelaesboo.telaPrincipal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.testetelaesboo.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class TelaPrincipal extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tela_principal);
-       getSupportActionBar().hide();
 
+        //Configuração do fragment do menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // tirar o título da toolbar
 
+        drawer = findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //NavigationView eventos dos fragmentos
+        NavigationView Navigationview = findViewById(R.id.nav_view);
+        Navigationview.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().
+                    replace(R.id.fragment_menu_tela_principal, new fragment_inicio()).commit();
+            Navigationview.setCheckedItem(R.id.inicio);
+        }
+
+        //Intent da tela de Login
         Intent recebetelaprincipal = getIntent();
     }
+
+    //NavigationView event
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.inicio:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_inicio()).commit();
+                break;
+            case R.id.perfil:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_perfil()).commit();
+                break;
+            case R.id.atividades:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_atividade()).commit();
+                break;
+            case R.id.configuracao:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_configuracao()).commit();
+                break;
+            case R.id.contatos:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_contatos()).commit();
+                break;
+            case R.id.grupos:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_grupo()).commit();
+                break;
+            case R.id.sobre:
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.fragment_menu_tela_principal, new fragment_sobre()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //Configuração do fragment do menu evento de saída e entrada
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
